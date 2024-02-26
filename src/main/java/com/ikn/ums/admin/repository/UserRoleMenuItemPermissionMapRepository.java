3dd90c5ed4d91846914a30abbd08df57450a8db2
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2024-02-26 13:32:13 +0530

    changed jar file name in pom.xml

--- src/main/java/com/ikn/ums/admin/repository/UserRoleMenuItemPermissionMapRepository.java
+++ src/main/java/com/ikn/ums/admin/repository/UserRoleMenuItemPermissionMapRepository.java
@@ -1,6 +1,8 @@
 package com.ikn.ums.admin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ikn.ums.admin.entity.UserRoleMenuItemPermissionMap;
@@ -13,5 +15,9 @@ public interface UserRoleMenuItemPermissionMapRepository extends JpaRepository<U
 	List<UserRoleMenuItemPermissionMap> findByEmail(String email);
 	
 	void deleteByEmail(String email);
+	
+	@Modifying
+	@Query("DELETE FROM UserRoleMenuItemPermissionMap WHERE roleId=:roleId AND menuItemIdList=:menuItemId")
+	void deleteUserRoleMenuItemPermissionMapOfUserByMenuItemId(Long roleId, String menuItemId);
 
 }
